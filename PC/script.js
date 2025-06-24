efecd481e4ea9b1bc39ee03fdb92bf8138ed3991
HENRIQUE GABRIEL DE OLIVEIRA <henrique07.oliveira@catolicasc.edu.br>
2025-06-24 20:59:42 -0300

    inova

--- PC/script.js
+++ PC/script.js
@@ -7,3 +7,14 @@ function adicionarAoCarrinho(produto) {
   function minhaFuncao() {
     alert('Imagem clicada!');
   }
+
+  <script>
+  function scrollCarrossel(direcao) {
+    const carrossel = document.getElementById("carrossel");
+    const larguraItem = 200; // tamanho do item + gap
+    carrossel.scrollBy({
+      left: direcao * larguraItem,
+      behavior: 'smooth'
+    });
+  }
+</script>
